dd68e77c77540ade24ca5bd84327156b174274d4
ahona <vikieahona@outlook.com>
2025-02-26 17:07:11 +0300

    made changes to the app

--- setup.py
+++ setup.py
@@ -1,48 +1,18 @@
-# from setuptools import setup, find_packages
-
-# setup(
-#     name="data-eng-cli",
-#     version="0.1",
-#     packages=find_packages(),
-#     install_requires=[
-#         "click",
-#         "pandas",
-#         "requests",
-#         "pyyaml"
-#     ],
-#     entry_points={
-#         "console_scripts": [
-#             "data-eng-cli = data_eng_cli.cli:cli"
-#         ]
-#     },
-# )
 from setuptools import setup, find_packages
 
 setup(
     name="data-eng-cli",
-    version="0.1.0",
-    author="Vic Kibira",
-    author_email="victorahonakibira@gmail.com",
-    description="A simple CLI tool for data engineering tasks",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/vicKibira/CLI",
+    version="0.1",
     packages=find_packages(),
     install_requires=[
         "click",
-        "sqlalchemy",
-        "duckdb",
-        "pymysql"
+        "pandas",
+        "requests",
+        "pyyaml"
     ],
     entry_points={
         "console_scripts": [
-            "data-eng-cli=data_eng_cli.cli:main",
-        ],
+            "data-eng-cli = data_eng_cli.cli:cli"
+        ]
     },
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires=">=3.7",
 )
